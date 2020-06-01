d70273ed6e1201c73796e68d5bac3f6af891462c
nemanjakontic <nemanja.kontic28@gmail.com>
2020-06-01 14:02:24 +0200

    Inicijalizacija projekta

---
+++ oglasiBackend/src/main/java/com/mn/oglasiBackend/OglasiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mn.oglasiBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OglasiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OglasiBackendApplication.class, args);
+	}
+
+}
---
+++ oglasiBackend/src/test/java/com/mn/oglasiBackend/OglasiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mn.oglasiBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OglasiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
